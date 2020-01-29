e01624f1abffd86b134b6a32ac321c18a4ac148d
maikofelix47 <maikofelix89@gmail.com>
2020-01-29 18:38:39 +0300

    Initialized Repo and added sum function to index.js

---
+++ README.md
@@ -0,0 +1,13 @@
+# PR Practice Repository
+
+This repository is intended to help new people to OSS and commonly used git commands
+
+## Set Up
+ 1. Fork the project
+ 2. Clone the project
+ 3. Configure upstream remote
+ 4. Checkout a branch
+ 5. Make changes
+ 6. Push to origin
+ 7. Create a PR
+ 8. Have the PR reviewed for merging
---
+++ index.js
@@ -0,0 +1,13 @@
+const sumValues = (a,b)=>{
+   console.log('Adding...Please wait');
+   return a + b;
+}
+
+const a = 2;
+const b = 3;
+const sum = sumValues(a,b);
+console.log('First no', a);
+console.log('Second no', b);
+console.log('----------------------------');
+console.log('Sum', sum);
+console.log('=============================');
