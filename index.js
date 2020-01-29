74985c92590e69fc054119a05ce3047bb7127fdf
maikofelix47 <maikofelix89@gmail.com>
2020-01-29 20:25:51 +0300

    Added Subtract function

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const sumValues = (a,b)=>{
    return a + b;
 }
 
+const subtractValues = (a,b)=> {
+   console.log('Subtracting....please wait');
+   return (a-b);
+}
+
 const a = 2;
 const b = 3;
 const sum = sumValues(a,b);
@@ -10,4 +15,11 @@ console.log('First no', a);
 console.log('Second no', b);
 console.log('----------------------------');
 console.log('Sum', sum);
+console.log('=============================');
+
+const difference = subtractValues(b,a);
+console.log('First no', b);
+console.log('Second no', a);
+console.log('----------------------------');
+console.log('Difference', difference);
 console.log('=============================');
