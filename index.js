35d37980bdaff38f5b5fb02a68b85a354380e6e1
maikofelix47 <maikofelix89@gmail.com>
2020-02-08 18:30:20 +0300

    Added Average function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const sumValues = (a,b)=>{
    console.log('Adding...Please wait');
    return a + b;
 }
+const averageValues = (a,b)=>{
+     return ((a+b)/2);
+}
 
 const a = 2;
 const b = 3;
@@ -10,4 +13,8 @@ console.log('First no', a);
 console.log('Second no', b);
 console.log('----------------------------');
 console.log('Sum', sum);
+console.log('=============================');
+const average = averageValues(a,b);
+console.log('----------------------------');
+console.log('Average', average);
 console.log('=============================');
